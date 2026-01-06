49d43e3f2ba40ed6d97fb6c8e1905baa20b37600
Hyunwook-Jeon <hyunwook6457@gmail.com>
2026-01-06 12:17:34 +0900

    Update ［PCCE 기출문제］ 9번 ／ 지폐 접기.py

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.py
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.py
@@ -16,3 +16,17 @@ def solution(wallet, bill):
             bill[0] = bill[1]
             bill[1] = c        
     return answer
+
+'''
+추가공부 : sorted(wallet)--> 을 사용하면 오름차순 정렬이 가능함.
+def solution(wallet, bill):
+    answer = 0
+    wallet = sorted(wallet)
+    bill = sorted(bill)
+    while wallet[0]<bill[0] or wallet[1]<bill[1]:
+        bill[-1] //=2
+        bill=sorted(bill)
+        answer+=1        
+    return answer
+이런식으로 코드를 구성하면 훨씬 쉽게 작성할 수 있음.
+'''
